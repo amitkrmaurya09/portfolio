2ddc88d1af3125d1cfb13fc662d64c021487ceed
Amit Kumar <amitkrmaurya09@gmail.com>
2024-09-03 19:20:01 +0530

    size

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
   <div id="about" className="h-screen px-28  flex items-center justify-center">
     <div  className="w-full p-32 flex shadow-2xl shadow-white justify-around items-center  rounded-lg " >
       <div className="bg-gray- p-5 rounded-lg">
-        <img className="h-fit w-fit pb-3"  src={StudentLogo} alt="student logo" />
+        <img className="h-52 w-52 pb-3"  src={StudentLogo} alt="student logo" />
         <h2 className="font-bold">Currently Pursuing B-tech from <br />Central university of Uttarakhand</h2>
       </div>
       <div className="w-1/2 h-fit p-5 ">
